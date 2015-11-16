e624249bc1bb11a0d7a59fb9894dad38662e4143
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-11-16 01:12:36 -0600

    Minor edits, add some functions XD

--- osm/mv_files.sh
+++ osm/mv_files.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 chmod +x get_josm.sh
+chmod +x update_josm.sh
 chmod +x latest-josm.sh
 chmod +x tested-josm.sh
-cp get_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
+cp get_josm.sh update_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
