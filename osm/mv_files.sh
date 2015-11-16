918287c3cfa52dcb35054b535a86afab5db7ca4b
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-11-16 01:01:05 -0600

    Minor edit

--- osm/mv_files.sh
+++ osm/mv_files.sh
@@ -2,4 +2,4 @@
 chmod +x get_josm.sh
 chmod +x latest-josm.sh
 chmod +x tested-josm.sh
-mv get_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
+cp get_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
