d107889b9a2c45aad56ae065af3675b5f7a69d1f
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-11-16 01:04:36 -0600

    Deleted to lines

--- osm/get_josm.sh
+++ osm/get_josm.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-rm -rf ~/.josm/jar/josm-tested.jar
-rm -rf ~/.josm/jar/josm-latest.jar
+mkdir ~/.josm/jar/
 axel -n 10 https://josm.openstreetmap.de/josm-tested.jar -o ~/.josm/jar/josm-tested.jar
 axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.josm/jar/josm-latest.tar
