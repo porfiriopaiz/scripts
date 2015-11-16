6a13a44c710373828fc3ecd173994f09a152939c
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-11-16 01:09:07 -0600

    Done!

---
+++ osm/update_josm.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+rm -rf ~/.josm/jar/josm-tested.jar
+rm -rf ~/.josm/jar/josm-latest.jar
+axel -n 10 https://josm.openstreetmap.de/josm-tested.jar -o ~/.josm/jar/josm-tested.jar
+axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.josm/jar/josm-latest.tar
