f614bf28c94e9644f315113ad2b470a0efe97093
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-11-15 23:47:23 -0600

    Added a reference link

--- fedora/set_default-win-grub2.sh
+++ fedora/set_default-win-grub2.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# Lectura recomendada:
+# https://ask.fedoraproject.org/en/question/8885/how-can-i-change-default-operating-system-in-start-up-boot-menu/
 grub2-set-default "Windows 7 (loader) (on /dev/sda1)"
 grub2-editenv list
 grub2-mkconfig -o /boot/grub2/grub.cfg
