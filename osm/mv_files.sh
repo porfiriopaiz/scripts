289f34c0d1cb4a4901b66c3cdc7b83e228b0398f
Porfirio Paiz <porfiriopaiz@gmail.com>
2015-12-17 00:58:44 -0600

    Changed from cp to mv

--- osm/mv_files.sh
+++ osm/mv_files.sh
@@ -3,4 +3,4 @@ chmod +x get_josm.sh
 chmod +x update_josm.sh
 chmod +x latest-josm.sh
 chmod +x tested-josm.sh
-cp get_josm.sh update_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
+mv get_josm.sh update_josm.sh latest-josm.sh tested-josm.sh /usr/local/bin/
