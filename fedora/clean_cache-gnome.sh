4df444760c94cd4e73e9b46bb09729d64bac5178
porfiriopaiz <porfiriopaiz@gmail.com>
2016-01-26 12:31:22 -0600

    addd clean cache gnome :D

---
+++ fedora/clean_cache-gnome.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+rm -rf /home/porfirio/.cache/thumbnails/fail/gnome-thumbnail-factory/*.png
+rm -rf /home/porfirio/.cache/thumbnails/large/*.png
+rm -rf /home/porfirio/.cache/thumbnails/normal/*.png
+rm -rf /home/porfirio/.local/share/recently-used.xbel
