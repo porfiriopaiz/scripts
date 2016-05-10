958c9f6e3520cbccb87ab10b8781f2a5265a629e
Porfirio Andrés Páiz Carrasco <porfiriopaiz@gmail.com>
2016-05-10 01:46:27 -0600

    Fixed a typo

--- osm/update_josm.sh
+++ osm/update_josm.sh
@@ -2,4 +2,4 @@
 rm -rf ~/.josm/jar/josm-tested.jar
 rm -rf ~/.josm/jar/josm-latest.jar
 axel -n 10 https://josm.openstreetmap.de/josm-tested.jar -o ~/.josm/jar/josm-tested.jar
-axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.josm/jar/josm-latest.tar
+axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.josm/jar/josm-latest.jar
