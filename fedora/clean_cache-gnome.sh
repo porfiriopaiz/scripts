f3e32ea257be1b2945a156b09f70f6dcfc80977d
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-08-05 14:18:40 -0600

    Add more files to delete from cache.

--- fedora/clean_cache-gnome.sh
+++ fedora/clean_cache-gnome.sh
@@ -2,4 +2,4 @@
 rm ~/.cache/thumbnails/fail/gnome-thumbnail-factory/*.png
 rm ~/.cache/thumbnails/large/*.png
 rm ~/.cache/thumbnails/normal/*.png
-rm ~/.local/share/recently-used.xbel
+rm ~/.local/share/recently-used.xbel.*
