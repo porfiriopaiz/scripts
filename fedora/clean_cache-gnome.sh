01feaf3d21ae1206b43645b9bbc4257f4f9ba0b8
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-08-05 16:43:45 -0600

    Add minor fix to clean_cache-gnome.sh

--- fedora/clean_cache-gnome.sh
+++ fedora/clean_cache-gnome.sh
@@ -2,4 +2,5 @@
 rm ~/.cache/thumbnails/fail/gnome-thumbnail-factory/*.png
 rm ~/.cache/thumbnails/large/*.png
 rm ~/.cache/thumbnails/normal/*.png
+rm ~/.local/share/recently-used.xbel
 rm ~/.local/share/recently-used.xbel.*
