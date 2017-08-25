5efadacf1ace6d6ee8dff83f6b754885cd2c4128
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2017-08-24 18:46:34 -0600

    Add minor fix.

--- osm/get_josm.sh
+++ osm/get_josm.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir ~/.cache/JOSM/jar/
-axel -n 10 https://josm.openstreetmap.de/josm-tested.jar -o ~/.cache/JOSM/jar/
-axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.cache/JOSM/jar/
+axel -n 10 https://josm.openstreetmap.de/josm-tested.jar -o ~/.cache/JOSM/jar/josm-tested.jar
+axel -n 10 https://josm.openstreetmap.de/josm-latest.jar -o ~/.cache/JOSM/jar/josm-latest.jar
