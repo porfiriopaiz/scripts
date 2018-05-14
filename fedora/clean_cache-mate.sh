64faba4cbacecdc89443cdf6893885baaf19a8a6
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2018-05-14 14:35:12 -0600

    Add property to delete recent accessed files and directories from history.

--- fedora/clean_cache-mate.sh
+++ fedora/clean_cache-mate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -rf ~/.cache/thumbnails/fail/mate-thumbnail-factory/*.png
 rm -rf ~/.cache/thumbnails/normal/*.png
+rm ~/.local/share/recently-used.xbel
+rm ~/.local/share/recently-used.xbel.*
