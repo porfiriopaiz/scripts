2679e609d975787ac19eba9d5818d934b3122482
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2018-05-17 22:44:51 -0600

    Improves i3wm scripts.

--- fedora/netinstall/i3wm/02-su.sh
+++ fedora/netinstall/i3wm/02-su.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 dnf group install mate-desktop-environment
+dnf group install "Common NetworkManager Submodules"
+dnf group install "Core"
+dnf group install "Dial-up Networking Support"
+dnf group install "Fonts"
+dnf group install "Guest Desktop Agents"
+dnf group install "Hardware Support"
+dnf group install "Multimedia"
+dnf group install "Standard"
+dnf group install base-x
+dnf group install "Input Methods"
+dnf install i3
+dnf install lightdm-gtk
 systemctl set-default graphical.target
 systemctl enable lightdm.service
 reboot
--- fedora/netinstall/i3wm/03-pionen.sh
+++ fedora/netinstall/i3wm/03-pionen.sh
@@ -1,16 +1,9 @@
 #! /bin/bash
 dnf clean all
 dnf makecache
+su -c 'dnf install caja'
 gsettings set org.mate.caja.preferences default-sort-order type
-gsettings set org.mate.Marco.general center-new-windows true
-gsettings set org.mate.Marco.general allow-tiling true
-gsettings set org.mate.background show-desktop-icons false
-gsettings set org.mate.media-handling automount-open false
-mkdir git_repos
-mkdir projects
-su -c 'dnf -y remove xfburn'
-su -c 'dnf -y remove exaile parole'
-su -c 'dnf -y remove hexchat'
-su -c 'dnf -y remove dnfdragora'
+mkdir ~/git_repos
+mkdir ~/projects
 su -c 'dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
 su -c 'dnf check-update'
