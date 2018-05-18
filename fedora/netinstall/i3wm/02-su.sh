f9f5cba949ba31531a70ffcebebabfff2e8d241c
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2018-05-18 09:24:13 -0600

    Add friendly groups names to i3wm postinstall scripts.

--- fedora/netinstall/i3wm/02-su.sh
+++ fedora/netinstall/i3wm/02-su.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
-dnf group install mate-desktop-environment
-dnf group install "Common NetworkManager Submodules"
-dnf group install "Core"
-dnf group install "Dial-up Networking Support"
-dnf group install "Fonts"
-dnf group install "Guest Desktop Agents"
-dnf group install "Hardware Support"
-dnf group install "Multimedia"
-dnf group install "Standard"
-dnf group install base-x
-dnf group install "Input Methods"
-dnf install i3
-dnf install lightdm-gtk
+dnf -y group install networkmanager-submodules
+dnf -y group install core
+dnf -y group install dial-up
+dnf -y group install fonts
+dnf -y group install guest-desktop-agents
+dnf -y group install hardware-support
+dnf -y group install multimedia
+dnf -y group install standard
+dnf -y group install base-x
+dnf -y group install input-methods
+dnf -y install fedora-icon-theme
+dnf -y install gnome-icon-theme
+dnf -y install gnome-icon-theme-extras
+dnf -y install i3
+dnf -y install lightdm-gtk
 systemctl set-default graphical.target
 systemctl enable lightdm.service
 reboot
