04f90542376b6152978de6dea0de86370f3a2add
Porfirio A. Páiz Carrasco <porfiriopaiz@gmail.com>
2018-08-05 02:26:40 -0600

    Add general and backup for many gits repos.

---
+++ general/bckp_git.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+tar -cvf ~/blog.tar ~/git_repos/blog
+tar -cvf ~/blog-hu3VObil.tar ~/.virtualenvs/blog-hu3VObil
+tar -cvf ~/site.tar ~/git_repos/site
+tar -cvf ~/site-8AhMD6NY.tar ~/.virtualenvs/site-8AhMD6NY
+tar -cvf ~/Apuntesrust.tar ~/git_repos/Apuntesrust
+tar -cvf ~/dotfiles.tar ~/git_repos/dotfiles
+tar -cvf ~/dsacpp.tar ~/git_repos/dsacpp
+tar -cvf ~/fedora.tar ~/git_repos/fedora
+tar -cvf ~/python3_opp.tar ~/git_repos/python3_opp
+tar -cvf ~/scripts.tar ~/git_repos/scripts
